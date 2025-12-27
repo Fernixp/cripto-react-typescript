cffd876a07c0fa26cab0e2899203ce400851c1bb
Fernando Nina <Fernandonina611@gmail.com>
2025-12-27 14:15:18 -0400

    feat: :tada: configuracion inicial del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+        <h1>Cripto App - Bienvenido a tu aplicación de criptomonedas</h1>
+        <p>Explora el mundo de las criptomonedas con nuestra plataforma intuitiva y segura.</p>
+    </>
+  )
+}
+
+export default App
