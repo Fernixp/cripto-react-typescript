0673237a5c2784a4e24bb8175b19a7c214334b67
Fernando Nina <Fernandonina611@gmail.com>
2026-01-04 08:24:03 -0400

    refactor: ♻️ moviendo el fetch de datos a service para reutilizacion y buenas practicas

---
+++ src/services/CryptoService.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { CryptoCurrenciesResponseSchema } from "../schema/crypto-schema";
+
+export async function getCryptos() {
+  const url =
+    "https://data-api.coindesk.com/asset/v1/top/list?page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC&toplist_quote_asset=USD&api_key=MiApiKey";
+
+  const {
+    data: {
+      Data: { LIST: cryptos },
+    },
+  } = await axios.get(url);
+  const result = CryptoCurrenciesResponseSchema.safeParse(cryptos);
+  if (result.success) {
+    return result.data;
+  }
+  throw new Error("Invalid data format");
+}
--- src/store.ts
+++ src/store.ts
@@ -1,25 +1,13 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import axios from 'axios';
-import { CryptoCurrenciesResponseSchema } from "./schema/crypto-schema";
 import type { CryptoCurrency } from "./types";
+import { getCryptos } from "./services/CryptoService";
 
 /* Definiendo el tipo para el estado */
 type CryptoState = {
   cryptocurrencies: CryptoCurrency[];
   fetchCryptos: () => Promise<void>
 };
-
-async function getCryptos(){
-    const url = 'https://data-api.coindesk.com/asset/v1/top/list?page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC&toplist_quote_asset=USD&api_key=MiApiKey';
-    
-    const { data: { Data: { LIST: cryptos } } } = await axios.get(url);
-    const result = CryptoCurrenciesResponseSchema.safeParse(cryptos);
-    if(result.success){
-        return result.data;
-    }
-    throw new Error('Invalid data format');
-}
 
 export const useCryptoStore = create<CryptoState>()(devtools((set) => ({
     cryptocurrencies: [],
